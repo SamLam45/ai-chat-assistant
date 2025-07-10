1f672cbb17d5a1ce36b708b28d45d165749be794
SamLam45 <lamsam5834@gmail.com>
2025-07-10 14:39:54 +0800

    update

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -140,8 +140,8 @@ export default function Blog({ user }: BlogProps) {
             </div>
             {user ? (
               <div className="dropdown">
-                <button className="btn btn-outline-primary px-4 fw-bold" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false" onClick={() => router.push('/login')}>
-                  個人檔案
+                <button className="btn btn-primary rounded-circle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false" onClick={() => router.push('/login')}>
+                  <i className="fas fa-user"></i>
                 </button>
                 <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
                   <li><span className="dropdown-item-text">{user.email}</span></li>
--- pages/training.tsx
+++ pages/training.tsx
@@ -145,8 +145,8 @@ export default function Training({ user }: TrainingProps) {
             </div>
             {user ? (
               <div className="dropdown">
-                <button className="btn btn-outline-primary px-4 fw-bold" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false" onClick={() => router.push('/login')}>
-                  個人檔案
+                <button className="btn btn-primary rounded-circle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false" onClick={() => router.push('/login')}>
+                  <i className="fas fa-user"></i>
                 </button>
                 <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
                   <li><span className="dropdown-item-text">{user.email}</span></li>
